a881be6b695c289abdc4788be82e47f6e5504ac8
Thomas Maier <contact@thomas-maier.net>
2022-01-09 17:14:04 +0100

    swap x,y order in alpha arrays

--- icons/power_icon_test.go
+++ icons/power_icon_test.go
@@ -10,9 +10,9 @@ func TestDrawPowerIcon(t *testing.T) {
 	dc := gg.NewContext(128, 128)
 	dx := 32
 	dy := 32
-	for y := 0; y < 64; y++ {
-		for x := 0; x < 64; x++ {
-			a := Power.Alpha64x64[y*64+x]
+	for x := 0; x < 64; x++ {
+		for y := 0; y < 64; y++ {
+			a := Power.Alpha64x64[x*64+y]
 			if a > 0 {
 				dc.SetRGBA(1, 1, 1, float64(a)/255)
 				dc.SetPixel(x+dx, y+dy)
